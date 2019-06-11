25df1a9daf9e6c5b0b4830d932e9852ba66008ca
Jose_Medolago <jrmedolago@gmail.com>
2019-06-11 13:59:55 -0300

    PROGRAMACAO FOR
    EXERCICIOS EM SALA DE AULA -TABUADA E DISTRIBUIÇÃO DE GRÃOS CONFORME TABULEIRO DE XADREZ

---
+++ EXERCICIO FOR_TABUADA/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+
+            Console.WriteLine("********** TABUADA **********");
+
+            Console.Write("\nDIGITE A TABUADA DESEJADA: ");
+            N = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n-----------------------------");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("{0} X {1} = {2}", N, i, N * i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EXERCICIO_FOR_GRAOS NO TABULEIRO/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCICIO_RAINHA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DESENVOLVA UM ALGORITMO QUE MOSTRE A QUANTIDADE TOTAL DE GRÃOS NUM TABULEIRO DE XADREZ DE 64 POSIÇÕES, SABENDO-SE QUE A PARTIR DA SEGUNDA CASA A QTDE DE GRÃOS DOBRAM.
+
+
+            double totalGraos = 1, somaGraos = 0;
+            int i;
+
+            Console.WriteLine();
+            Console.WriteLine("********** DADOS COLHEITA **********");
+            Console.WriteLine("\nCALCULAR O TOTAL DE GRÃOS");
+
+            for (i = 1; i <= 64; i++)
+            {
+
+                if (i > 1)
+                {
+                    totalGraos = totalGraos * 2;
+                }
+                
+                Console.WriteLine("\nCasa {0} = {1:N0}", i, totalGraos);
+                somaGraos = somaGraos + totalGraos;
+                                            
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("\nTOTAL GERAL = {0:n0}", somaGraos);
+
+            Console.ReadKey();
+        }
+    }
+}
