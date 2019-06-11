5a0449a8eb857828b416eaf2b3058a540398b6b5
Jose_Medolago <jrmedolago@gmail.com>
2019-06-11 14:28:59 -0300

    PROGRAMACAO IF
    EXERCICIOS EM SALA PARA DETERMINAR SE O NUMERO INFORMADO É PAR OU IMPAR

---
+++ EXERCICIO IF_PAR, IMPAR/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_divisivel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1;
+
+            Console.WriteLine();
+            Console.WriteLine("\n************** INFORMAÇÕES **************");
+            Console.Write("\nDIGITE UM NÚMERO QUALQUER..: ");
+            n1 = double.Parse(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine();
+            
+            if (n1 % 2 == 0)
+            {
+                Console.WriteLine("\nO NUMERO INFORMADO É PAR");
+            }
+
+            else
+            {
+                Console.WriteLine("\nO NÚMERO INFORMADO É IMPAR");
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
