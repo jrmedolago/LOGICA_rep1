52e0c0843e290a92fdb485721cb5f0f431510289
Jose_Medolago <jrmedolago@gmail.com>
2019-06-11 12:03:50 -0300

    PROMAÇÃO WHILE
    EXERCICIOS PARA CONDIÇÃO WHILE - VERIFICAR MAIOR NUMERO, SOMAR E MOSTRAR A MÉDIA DOS VALORES

---
+++ EXERCICIO WHILE_MAIOR NUMERO/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WHILE_MAIOR_NUMERO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CRIE UM ALGORITMO QUE LEIA 5 POSIÇÕES E MOSTRE O MAIOR VALOR ENTRE ELES.
+
+
+            double NUM, maior = 0, cont = 1;
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("\n********* REGISTRO ************");
+            Console.BackgroundColor = ConsoleColor.Black;
+            
+
+            while ( cont <= 5)
+            {
+                Console.Write("\nINFORME O {0}º NÚMERO: ", cont);
+                NUM = double.Parse(Console.ReadLine());
+
+                if (NUM > maior)
+                {
+                    maior = NUM++;
+                }
+
+                cont++;
+            }
+                        
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("\n********** RESULTADO **********");
+            Console.WriteLine("\nO MAIOR NÚMERO => " + maior);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ EXERCICIO WHILE_MOSTRA 10 NUM, SOMA E MEDIA/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WHILE_10_NUMEROS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //desenvolva um algoritmo que leia 5 números, mostre a soma e a média de todos.
+
+            double num, soma = 0, media = 0, cont = 1;
+
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("******* INFORMAÇÕES BÁSICAS **********");
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            while ( cont <= 5 )
+            {
+                Console.Write("\nINFORME O {0}º NUMERO: ", cont);
+                num = double.Parse(Console.ReadLine());
+
+                soma = soma + num++;
+
+
+
+                cont++;
+            }
+            media = soma / 5;
+
+
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Clear();
+
+            Console.WriteLine("\n********** RESULTADOS **********");
+            Console.WriteLine("\nA SOMA DOS VALORES É: {0:N}", soma);
+            Console.WriteLine("\nA MEDIA DOS VALORES É: {0:N}", media);
+
+            Console.ReadKey();
+
+        }
+    }
+}
